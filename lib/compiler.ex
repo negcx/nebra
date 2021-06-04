060bd7ec9c2aaa1bfb0be726ddc764e574bd6198
Kyle Johnson <kyle@plateiq.com>
2021-06-04 08:03:48 -0600

    Significantly simpler . implementation

--- lib/compiler.ex
+++ lib/compiler.ex
@@ -22,35 +22,7 @@ defmodule Compiler do
   def compile([child | ast]), do: compile(child) <> compile(ast)
   def compile([]), do: ""
 
-  def compile({:=, _metadata, [left, right]}) do
-    stack = assign_stack(left) |> IO.inspect(label: "Stack")
-    output = stack |> compile_assign_stack(compile(right))
-    root_item = Enum.reverse(stack) |> hd()
-
-    "Map.get(#{@symbol_t} = (#{output}), #{root_item})"
-  end
-
-  def compile({:., _, _} = node) do
-    access =
-      dot_stack(node)
-      |> Enum.reverse()
-      |> Enum.map(fn item ->
-        "Map.get(#{item})"
-      end)
-      |> Enum.join(" |> ")
-
-    "(s |> #{access})"
-  end
-
-  def compile({:access, _, [{:id, _, id}, right]}) do
-    "(s |> Map.get(\"#{id}\") |> Map.get(#{compile(right)}))"
-  end
-
-  def compile({:access, _, [left, right]}) do
-    "(s |> #{compile(left)} |> Map.get(#{compile(right)}))"
-  end
-
-  def compile({:id, _metadata, id}), do: "Map.get(#{@symbol_t}, \"#{id}\")"
+  def compile({:id, _metadata, id}), do: "#{@symbol_t}[\"#{id}\"]"
 
   def compile({:+, _, [left, right]}),
     do: compile(left) <> " + " <> compile(right)
@@ -96,42 +68,8 @@ defmodule Compiler do
     "(fn #{fn_params} -> #{binding_body}\n#{compile(body)} end)"
   end
 
-  # Handle the case of assigning to a multi-nested map
-  # on the left hand side. i.e. x.y.z = 3
-  def dot_stack({:., _, [left, right]}) do
-    dot_stack(right) ++ dot_stack(left)
-  end
+  def compile({:., _, [left, {:id, _, right_id}]}),
+    do: "#{compile(left)}[#{quotes(right_id)}]"
 
-  def dot_stack({:id, _, id}), do: ["\"#{id}\""]
-
-  def dot_stack(node), do: [compile(node)]
-
-  def assign_stack({:., _, [left, right]}) do
-    assign_stack(right) ++ assign_stack(left)
-  end
-
-  def assign_stack({:access, _, [left, right]}) do
-    assign_stack(right) ++ assign_stack(left)
-  end
-
-  def assign_stack({:id, _, id}), do: ["\"#{id}\""]
-
-  def assign_stack(node), do: [compile(node)]
-
-  def compile_assign_stack([head], right),
-    do: "#{@symbol_t} |> Map.put(#{head}, #{right})"
-
-  def compile_assign_stack([head | tail], right) do
-    path =
-      Enum.reverse(tail)
-      |> Enum.map(fn item ->
-        " |> Map.get(#{item}, %{})"
-      end)
-
-    compile_assign_stack(tail, "s #{path} |> Map.put(#{head}, #{right})")
-  end
-
-  def compile_access({:access, _, [{:id, _, id}, right]}) do
-    "Map.get(\"#{id}\") |> Map.get(#{compile_access(right)})"
-  end
+  defp quotes(s), do: "\"#{s}\""
 end
--- test/compiler_test.exs
+++ test/compiler_test.exs
@@ -1,20 +1,36 @@
 defmodule CompilerTest do
   use ExUnit.Case
 
-  test "Complex map access" do
-    assert {"Hello, it works.", _} =
-             Nebra.go("""
-             x["hello"] = "greeting";
-             y.greeting = "Hello, it works.";
-             y[x.hello]
-             """)
+  test "Basic . pattern" do
+    assert Nebra.compile("x1.x2.x3.x4") ==
+             """
+             s = %{}
+             s["x1"]["x2"]["x3"]["x4"]
+             """
+             |> String.trim_trailing("\n")
 
-    assert {"A demon wrote this code", _} =
-             Nebra.go("""
-             (x)["hello"] = "greeting";
-             (y).greeting = "Hello, it works.";
-             (((y)[(x).hello]).x)[(y.greeting)] = "A demon wrote this code";
-             (((y)["greeting"])["x"])["Hello, it works."]
-             """)
+    assert Nebra.compile("blah().x1.x2") ==
+             """
+             s = %{}
+             s["blah"].()["x1"]["x2"]
+             """
+             |> String.trim_trailing("\n")
+  end
+
+  test "Complex map access" do
+    # assert {"Hello, it works.", _} =
+    #          Nebra.go("""
+    #          x["hello"] = "greeting";
+    #          y.greeting = "Hello, it works.";
+    #          y[x.hello]
+    #          """)
+
+    # assert {"A demon wrote this code", _} =
+    #          Nebra.go("""
+    #          (x)["hello"] = "greeting";
+    #          (y).greeting = "Hello, it works.";
+    #          (((y)[(x).hello]).x)[(y.greeting)] = "A demon wrote this code";
+    #          (((y)["greeting"])["x"])["Hello, it works."]
+    #          """)
   end
 end
